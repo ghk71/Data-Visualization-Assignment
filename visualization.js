8b1fb14faf0cb00341ad97145c11efc5e1e08e5a
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2020-11-19 18:11:26 +0900

    sda

--- visualization.js
+++ visualization.js
@@ -1,23 +1,44 @@
-var svgHeight = 240;
-var barElements;
-var dataSet = [1.83, 3.63, 2.52, 9.09, 2.76, 1.28]
+var width = 900, height=300, pad=20, left_pad = 100;
 
-barElements = d3.select("#myGraph").selectAll("rect").data(dataSet)
+var x = d3.scale.ordinal().rangeRoundBands([left_pad, width-pad], 0.1);
+var y = d3.scale.linear().range([height-pad, pad]);
 
-barElements.enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("height", function(d,i){
-      return d * 10;
-    })
-    .attr("width", 20)
-    .attr("x", function(d,i){
-      return i*25
-    })
-    .attr("y", function(d,i) {
-      return svgHeight-d*10;
-    })
-    .selectAll("rect")
-    .data(dataSet)
-    .enter()
-    .append("rect")
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
+var yAxis = d3.svg.axis().scale(y).orient("left");
+
+var svg = d3.select("#graph").append("svg")
+            .attr("width", width).attr("height", height);
+
+d3.json('histogram-hours.json', function(data) {
+  data = d3.keys(data)
+          .map(function (key) {
+            return {
+              bucket: Number(key),
+              N: data[key]
+            };
+          });
+
+  x.domain(data.map(function (d) {return d.bucket;}));
+  y.domain([0, d3.max(data, function(d) {return d.N;})]);
+
+  svg.append("g")
+     .attr("class", "axis")
+     .attr("transform", "translate(0, "+(height-pad)+")")
+     .call(xAxis);
+
+  svg.append("g")
+     .attr("class", "axis")
+     .attr("transform", "translate(" +(left_pad-pad)+", 0)")
+     .call(yAxis);
+
+  svg.selectAll('rect')
+     .data(data)
+     .enter()
+     .append('rect')
+     .attr('class', 'bar')
+     .attr('x', function(d) {return x(d.bucket); })
+     .attr('width', x.rangeBand())
+     .attr('y', function(d) {return y(d.N); })
+     .attr('height', function(d) {return height-pad-y(d.N); });
+
+});
